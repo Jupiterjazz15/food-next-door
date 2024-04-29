b2b6ada712e72879b48f1f67932b1ad6eb9ba14e
clothilde scache <clothilde.sca@gmail.com>
2024-04-29 12:16:46 +0200

    added methods (index, show, new, create) and created views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,34 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show]
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :quantity_unit, :quantity_value, :available_start_date, :available_end_date, :best_before_date, :item_address, :food_condition, :home_condition, :is_available)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
