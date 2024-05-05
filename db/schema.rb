630eb3384a362ae9bf16ca5a6fa6f3b561ed1e81
clothilde scache <clothilde.sca@gmail.com>
2024-05-05 16:33:08 +0200

    added default value false to is_available in items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_27_184909) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_05_143112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_27_184909) do
     t.string "item_address"
     t.string "food_condition"
     t.string "home_condition"
-    t.boolean "is_available"
+    t.boolean "is_available", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_items_on_category_id"
