4f27b2b4926aa0a687019aabb461db14650715bb
coraliehaller <haller.coralie.15@gmail.com>
2024-05-06 09:23:27 +0200

    J'ajoute tous les champs à la table Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@ p "Building #{building1.name} has been created"
 building2 = Building.create!(name:"Bâtiment Vila Gaudelet", description: "Un bâtiment chaleureux et convivial où il fait bon vivre.", building_address: "16 Vila Gaudelet, 75011 Paris")
 p "Building #{building2.name} has been created"
 
-user1 = User.create!(first_name: "Clothile", last_name: "Scache", phone: "00-00-00-00-00", email: "clothilde@test.com", password: "azerty", user_address: "61 Rue Servan, 75011 Paris", is_ambassador:  true, want_to_be_ambassador: true, building_id: building1.id)
+user1 = User.create!(first_name: "Clothile", last_name: "Scache", birthday:"18/07/1996", phone: "00-00-00-00-00", user_address:"61 Rue Servan, 75011 Paris", email: "clothilde@test.com", password: "azerty", user_address: "61 Rue Servan, 75011 Paris", is_ambassador:  true, want_to_be_ambassador: true, building_id: building1.id)
 p "User #{user1.first_name} has been created"
 
-user2 = User.create!(first_name: "Coralie", last_name: "Haller", phone: "00-00-00-00-00", email: "coralie@test.com", password: "azerty", user_address: "16 Vila Gaudelet, 75011 Paris", is_ambassador: false, want_to_be_ambassador: false, building_id: building2.id)
+user2 = User.create!(first_name: "Coralie", last_name: "Haller",birthday:"15/08/1994", phone: "00-00-00-00-00", user_address: "16 Vila Gaudelet, 75011 Paris", email: "coralie@test.com", password: "azerty", user_address: "16 Vila Gaudelet, 75011 Paris", is_ambassador: false, want_to_be_ambassador: false, building_id: building2.id)
 p "User #{user2.first_name} has been created"
 
 ### Catégorie FOOD et ses sous catégories ###
