66a4870d87e3fffc6c6abaebec9c41f4bbe092b1
clothilde scache <clothilde.sca@gmail.com>
2024-05-07 18:48:59 +0200

    designed item card and item category and added them to the home view + had to create the model building and add users and buildings elements in the seed to be able to login

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 User.delete_all
 
-user1 = User.create!(first_name: "Clothile", last_name: "Sache", phone: "00-00-00-00-00", email: "clothilde@test.com", password: "azerty", neighbourhood_type: "Building")
+building1 = Building.create!(name: "Bâtiment rue Servan", description: "Un bâtiment à la décoration sobre où vivent des individus sympathiques et engagés.", building_address:"61 Rue Servan, 75011 Paris")
+p "Building #{building1.name} has been created"
+
+building2 = Building.create!(name:"Bâtiment Vila Gaudelet", description: "Un bâtiment chaleureux et convivial où il fait bon vivre.", building_address: "16 Vila Gaudelet, 75011 Paris")
+p "Building #{building2.name} has been created"
+
+user1 = User.create!(first_name: "Clothilde", last_name: "Scache", birthday:"18/07/1996", phone: "00-00-00-00-00", user_address:"61 Rue Servan, 75011 Paris", email: "clothilde@test.com", password: "azerty", is_ambassador:  true, want_to_be_ambassador: true, building_id: building1.id)
 p "User #{user1.first_name} has been created"
 
-user2 = User.create!(first_name: "Coralie", last_name: "Haller", phone: "00-00-00-00-00", email: "coralie@test.com", password: "azerty", neighbourhood_type: "Residential Street")
+user2 = User.create!(first_name: "Coralie", last_name: "Haller",birthday:"15/08/1994", phone: "00-00-00-00-00", user_address: "16 Vila Gaudelet, 75011 Paris", email: "coralie@test.com", password: "azerty", is_ambassador: false, want_to_be_ambassador: false, building_id: building2.id)
 p "User #{user2.first_name} has been created"
