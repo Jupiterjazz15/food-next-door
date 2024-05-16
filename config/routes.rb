12e8c47079d2c9f14286619089648ab774abc3e4
coraliehaller <haller.coralie.15@gmail.com>
2024-05-16 11:50:11 +0200

    Création de la route welcome + de la méthode welcome dans PagesController (avec modification pour que la vue welcome  apparaisse avant la demande d'indentification + création de la vue welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "pages#home"
+  root to: "pages#welcome"
   resources :items, only: %i[index show new create]
-  get "/dashboard", to: "pages#dashboard"
 
+  get "/home", to: "pages#home"
+  get "/dashboard", to: "pages#dashboard"
   get "up" => "rails/health#show", as: :rails_health_check
 end
