371af5d5740713b469f927b33e2e7dd869dd368a
coraliehaller <haller.coralie.15@gmail.com>
2024-06-18 11:16:32 +0200

    1 : category.rb => ajout des validations pertinentes pour mettre en place cloudinary // 2 :_card_subcategory.html.erb > ajustement de l'intégration des photos // 3 : seeds.rb => ajout des photos des catégories et sous catégories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,9 @@ class Category < ApplicationRecord
   has_many :items
   has_many :subcategories, class_name: 'Category', foreign_key: 'parent_id'
   belongs_to :parent_category, class_name: 'Category', optional: true
+  has_one_attached :photo
 
+  validates :photo, presence: true
   validates :name, presence: true
   validates :name, uniqueness: { scope: :parent_id }
 end
