8020166de6b7863763f95aec8342df06b8320a94
clothilde scache <clothilde.sca@gmail.com>
2024-09-09 16:09:48 +0200

    corrected form and deleted unused js form controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to item_path(@item), notice: 'Item was successfully created.'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
