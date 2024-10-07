98cfce64d31d3075c2375c48d8def24c0b7c1507
clothilde scache <clothilde.sca@gmail.com>
2024-10-07 15:13:44 +0200

    finalized modals design

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -12,23 +12,31 @@ export default class extends Controller {
     this.actionModal = null;
   }
 
+  // Afficher la modale de catégorie
   showCategoryModal() {
-    console.log("J'ai cliqué sur le bouton de la navbar")
-    // Affichage de la modal de catégorie
     this.categoryModalTarget.classList.remove("d-none")
   }
 
+  // Sélectionner la catégorie et afficher la modale d'action
   selectCategory(event) {
-    // Sélection de la catégorie et affichage de la modal d'action
     this.categoryModal = event.currentTarget.dataset.category
     this.categoryModalTarget.classList.add("d-none")
     this.actionModalTarget.classList.remove("d-none")
   }
 
+  // Sélectionner l'action et rediriger vers le formulaire de création d'un item
   selectAction(event) {
-    // Sélection de l'action et redirection vers le formulaire de création d'un item
     this.actionModal = event.currentTarget.dataset.type
     const url = `/items/new?category=${this.categoryModal}&type=${this.actionModal}`
     window.location.href = url
+  }
+
+  // Fermer la modale en masquant la modale actuelle
+  closeModal() {
+    if (!this.categoryModalTarget.classList.contains("d-none")) {
+      this.categoryModalTarget.classList.add("d-none")
+    } else if (!this.actionModalTarget.classList.contains("d-none")) {
+      this.actionModalTarget.classList.add("d-none")
+    }
   }
 }
