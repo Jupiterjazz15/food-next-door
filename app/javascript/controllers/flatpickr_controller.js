94097e4dd76f11ba1284b3e199082affd74c23e4
Clothilde Scache <clothilde.sca@gmail.com>
2024-11-21 14:57:15 +0100

    Refactor modal styles and update Flatpickr integration

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,17 +1,35 @@
 import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
-import rangePlugin from "flatpickrRangePlugin";
+// import rangePlugin from "flatpickrRangePlugin";
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   static targets = ['startDateInput', 'endDateInput', 'useByDateInput']
+
   connect() {
-    flatpickr(this.startDateInputTarget, {
-      "plugins": [new rangePlugin({ input: this.endDateInputTarget})]
-     })
+    console.log("Controller Flatpicker connected")
+
+    this.startPicker = flatpickr(this.startDateInputTarget, {
+      dateFormat: "d-m-Y",
+      mode: "range",
+      enableTime: false,
+      minDate: "today",
+      onChange: (selectedDates) => {
+        if (selectedDates.length === 2) {
+          this.startDateInputTarget.value = flatpickr.formatDate(selectedDates[0], "d-m-Y");
+          this.endDateInputTarget.value = flatpickr.formatDate(selectedDates[1], "d-m-Y");
+        }
+      }
+    });
+
     flatpickr(this.useByDateInputTarget, {
-      enableTime: true,
-      dateFormat: "Y-m-d",
-    })
+      dateFormat: "d-m-Y",
+    });
+
   }
+
+  openStartPicker() {
+    this.startPicker.open();
+  }
+
 }
